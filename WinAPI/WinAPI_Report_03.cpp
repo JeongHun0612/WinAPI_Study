44c4a4c208cdbe175c1e378e177a9f02935e1b65
jeonghun <sjh960612@gmail.com>
2023-06-13 03:58:00 +0900

    23/06/12 Report Result

--- WinAPI/WinAPI_Report_03.cpp
+++ WinAPI/WinAPI_Report_03.cpp
@@ -19,125 +19,57 @@
 #define SCR_WIDTH		800
 #define SCR_HEIGHT		800
 
+#define REPORT1			1
+#define REPORT2			2
+
+#define REPORT_TYPE		REPORT2
+
 HINSTANCE _hInstance;
 HWND _hWnd;
 LPTSTR _lpszClass = TEXT("Windows API");
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
-bool OnCollisionBox(int key, RECT rc1, RECT rc2)
+int width = 100;
+int height = 100;
+int sWidth = 50;
+int sHeight = 50;
+int dist = 5;
+
+RECT bigRC1 = RectMakeCenter(400, 400, width, height);
+RECT bigRC2 = RectMakeCenter(500, 400, width, height);
+RECT smallRC = RectMakeCenter(400, 400, sWidth, sHeight);
+
+void MoveToRect(int direction, RECT *rc)
 {
-	switch (key)
+	switch (direction)
 	{
 	case VK_UP:
-		if (rc1.top != rc2.bottom || rc1.bottom < rc2.top)
-		{
-			return false;
-		}
-		else
-		{
-			return (rc1.left < rc2.right) && (rc1.right > rc2.left);
-		}
+		rc->top -= dist;
+		rc->bottom -= dist;
+		break;
 	case VK_DOWN:
-		if (rc1.bottom != rc2.top || rc1.top > rc2.bottom)
-		{
-			return false;
-		}
-		else
-		{
-			return (rc1.left < rc2.right) && (rc1.right > rc2.left);
-		}
+		rc->top += dist;
+		rc->bottom += dist;
+		break;
 	case VK_LEFT:
-		if (rc1.left != rc2.right || rc1.right < rc2.left)
-		{
-			return false;
-		}
-		else
-		{
-			return (rc1.bottom > rc2.top) && (rc1.top < rc2.bottom);
-		}
+		rc->left -= dist;
+		rc->right -= dist;
+		break;
 	case VK_RIGHT:
-		if (rc1.right != rc2.left || rc1.left > rc2.right)
-		{
-			return false;
-		}
-		else
-		{
-			return (rc1.bottom > rc2.top) && (rc1.top < rc2.bottom);
-		}
-	default:
-		return false;
+		rc->left += dist;
+		rc->right += dist;
+		break;
 	}
+}
 
-	//if ((rc1.top == rc2.bottom || rc1.top <= rc2.bottom) && (rc1.left < rc2.right && rc1.right > rc2.left))
-	//{
-	//	return true;
-	//}
-	//else if ((rc1.top == rc2.bottom || rc1.bottom >= rc2.top) && (rc1.left < rc2.right && rc1.right > rc2.left))
-	//{
-	//	return true;
-	//}
-	//else if ((rc1.left == rc2.right || rc1.left <= rc2.right) && (rc1.bottom > rc2.top && rc1.top < rc2.bottom))
-	//{
-	//	return true;
-	//}
-	//else if ((rc1.left == rc2.right || rc1.right >= rc2.left) && (rc1.bottom > rc2.top && rc1.top < rc2.bottom))
-	//{
-	//	return true;
-	//}
+void TempRectAngle(RECT* destRC, RECT* sourRC)
+{
+	RECT temp;
 
-	//return false;
-
-
-	//switch (key)
-	//{
-	//case VK_UP:
-	//		return (rc1.top == rc2.bottom || rc1.top <= rc2.bottom) && (rc1.left < rc2.right&& rc1.right > rc2.left);
-	//case VK_DOWN:
-	//	if (rc1.bottom <= rc2.top)
-	//	{
-	//		return rc1.bottom >= rc2.top && (rc1.left < rc2.right&& rc1.right > rc2.left);
-	//	}
-	//case VK_LEFT:
-	//	if (rc1.left >= rc2.right)
-	//	{
-	//		return rc1.left <= rc2.right && (rc1.bottom > rc2.top && rc1.top < rc2.bottom);
-	//	}
-	//case VK_RIGHT:
-	//	if (rc1.right <= rc2.left)
-	//	{
-	//		return rc1.right >= rc2.left && (rc1.bottom > rc2.top && rc1.top < rc2.bottom);
-	//	}
-	//}
-
-	//return false;
-
-
-	//if (rc1.top <= rc2.bottom && (rc1.left < rc2.right && rc1.right > rc2.left))
-	//{
-	//	return true;
-	//}
-	//else if (rc1.bottom >= rc2.top && (rc1.left < rc2.right && rc1.right > rc2.left))
-	//{
-	//	return true;
-	//}
-	//else if (rc1.left <= rc2.right && (rc1.bottom > rc2.top && rc1.top < rc2.bottom))
-	//{
-	//	return true;
-	//}
-	//else if (rc1.right >= rc2.left && (rc1.bottom > rc2.top && rc1.top < rc2.bottom))
-	//{
-	//	return true;
-	//}
-
-	//return false;
-
-
-	/*if (rc1.top <= rc2.bottom && rc1.left < rc2.right && rc1.right > rc2.left)
-	if (rc1.bottom >= rc2.top && rc1.left < rc2.right && rc1.right > rc2.left)
-
-	if (rc1.left <= rc2.right && rc1.bottom > rc2.top && rc1.top < rc2.bottom)
-	if (rc1.right >= rc2.left && rc1.bottom > rc2.top && rc1.top < rc2.bottom)*/
+	temp = *destRC;
+	*destRC = *sourRC;
+	*sourRC = temp;
 }
 
 int APIENTRY WinMain(
@@ -179,136 +111,11 @@ int APIENTRY WinMain(
 	return (int)message.lParam;
 }
 
-//LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
-//{
-//	HDC hdc;
-//	PAINTSTRUCT ps;
-//	RECT playerRC;
-//	RECT enemyRC;
-//	RECT smallRC;
-//
-//	static int curPX = 300;
-//	static int curPY = 300;
-//	static int curEX = 500;
-//	static int curEY = 500;
-//	int width = 100;
-//	int height = 100;
-//	int dist = 10;
-//	static bool isPlayer = true;
-//
-//	playerRC = RectMakeCenter(curPX, curPY, width, height);
-//	enemyRC = RectMakeCenter(curEX, curEY, width, height);
-//	smallRC = RectMakeCenter(curPX, curPY, width / 2, height / 2);
-//
-//	switch (iMessage)
-//	{
-//	case WM_CREATE:
-//		break;
-//	case WM_PAINT:
-//		hdc = BeginPaint(hWnd, &ps);
-//
-//		DrawRectMake(hdc, playerRC);
-//		DrawRectMake(hdc, enemyRC);
-//		DrawRectMake(hdc, smallRC);
-//
-//		EndPaint(hWnd, &ps);
-//		break;
-//	case WM_KEYDOWN:
-//	{
-//		switch (wParam)
-//		{
-//		case VK_UP:
-//			if (curPY < 1 || curEY < 1) break;
-//
-//			if (curPY == curEY + height && curPX + width > curEX + 1 && curPX < curEX + width)
-//			{
-//				curPY = curEY;
-//			}
-//			curPY -= dist;
-//
-//			break;
-//		case VK_DOWN:
-//			if (curPY + height > SCR_HEIGHT - 40) break;
-//			if (isPlayer)
-//			{
-//				curPY += dist;
-//			}
-//			else
-//			{
-//				curEY += dist;
-//			}
-//
-//			if (curPY + height == curEY && curPX + width > curEX + 1 && curPX < curEX + width)
-//			{
-//				isPlayer = !isPlayer;
-//			}
-//			break;
-//		case VK_LEFT:
-//			if (curPX < 1) break;
-//
-//			if (isPlayer)
-//			{
-//				curPX -= dist;
-//			}
-//			else
-//			{
-//				curEX -= dist;
-//			}
-//
-//			if (curPX == curEX + width && curPY + height > curEY + 1 && curPY < curEY + height)
-//			{
-//				isPlayer = !isPlayer;
-//			}
-//			break;
-//		case VK_RIGHT:
-//			if (curPX + width > SCR_WIDTH - 17) break;
-//
-//			if (isPlayer)
-//			{
-//				curPX += dist;
-//			}
-//			else
-//			{
-//				curEX += dist;
-//			}
-//
-//			if (curPX + width == curEX && curPY + height > curEY + 1 && curPY < curEY + height)
-//			{
-//				isPlayer = !isPlayer;
-//			}
-//
-//			break;
-//		}
-//
-//		InvalidateRect(hWnd, NULL, TRUE);
-//		break;
-//	}
-//	case WM_DESTROY:
-//		PostQuitMessage(0);
-//		return 0;
-//	}
-//
-//	return DefWindowProc(hWnd, iMessage, wParam, lParam);
-//}
-
+#if REPORT_TYPE	== REPORT1
 LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
 {
 	HDC hdc;
 	PAINTSTRUCT ps;
-	RECT rc1;
-	RECT rc2;
-
-	static int curPX = 300;
-	static int curPY = 100;
-	static int curEX = 500;
-	static int curEY = 100;
-	int width = 100;
-	int height = 100;
-	int dist = 7;
-	static bool isPlayer = true;
-
-	rc1 = { curPX, curPY, curPX + width, curPY + height };
-	rc2 = { curEX, curEY, curEX + width, curEY + height };
 
 	switch (iMessage)
 	{
@@ -317,8 +124,9 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
 	case WM_PAINT:
 		hdc = BeginPaint(hWnd, &ps);
 
-		DrawRectMake(hdc, rc1);
-		DrawRectMake(hdc, rc2);
+		DrawRectMake(hdc, bigRC1);
+		DrawRectMake(hdc, bigRC2);
+		DrawRectMake(hdc, smallRC);
 
 		EndPaint(hWnd, &ps);
 		break;
@@ -327,44 +135,55 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
 		switch (wParam)
 		{
 		case VK_UP:
-			if (curPY < 1 || curEY < 1) break;
+			if (bigRC1.top < 1) break;
 
-			if (OnCollisionBox(VK_UP, rc1, rc2))
+			MoveToRect(VK_UP, &bigRC1);
+			MoveToRect(VK_UP, &smallRC);
+
+			if (abs(bigRC1.top - bigRC2.bottom) <= dist && (bigRC1.left < bigRC2.right) && (bigRC1.right > bigRC2.left))
 			{
-				curEY -= dist;
+				TempRectAngle(&bigRC1, &bigRC2);
+				smallRC = { bigRC1.left + sWidth / 2, bigRC1.top + sHeight / 2, bigRC1.right - sWidth / 2, bigRC1.bottom - sHeight / 2 };
 			}
-			curPY -= dist;
-
 			break;
+
 		case VK_DOWN:
-			if (curPY + height > SCR_HEIGHT - 40) break;
+			if (bigRC1.bottom > SCR_HEIGHT - 40) break;
 
-			if (OnCollisionBox(VK_DOWN, rc1, rc2))
+			MoveToRect(VK_DOWN, &bigRC1);
+			MoveToRect(VK_DOWN, &smallRC);
+
+			if (abs(bigRC1.bottom - bigRC2.top) <= dist && (bigRC1.left < bigRC2.right) && (bigRC1.right > bigRC2.left))
 			{
-				curEY += dist;
+				TempRectAngle(&bigRC1, &bigRC2);
+				smallRC = { bigRC1.left + sWidth / 2, bigRC1.top + sHeight / 2, bigRC1.right - sWidth / 2, bigRC1.bottom - sHeight / 2 };
 			}
-			curPY += dist;
-
 			break;
+
 		case VK_LEFT:
-			if (curPX < 1) break;
+			if (bigRC1.left < 1) break;
 
-			if (OnCollisionBox(VK_LEFT, rc1, rc2))
+			MoveToRect(VK_LEFT, &bigRC1);
+			MoveToRect(VK_LEFT, &smallRC);
+
+			if (abs(bigRC1.left - bigRC2.right) <= dist && (bigRC1.bottom > bigRC2.top) && (bigRC1.top < bigRC2.bottom))
 			{
-				curEX -= dist;
+				TempRectAngle(&bigRC1, &bigRC2);
+				smallRC = { bigRC1.left + sWidth / 2, bigRC1.top + sHeight / 2, bigRC1.right - sWidth / 2, bigRC1.bottom - sHeight / 2 };
 			}
-			curPX -= dist;
-
 			break;
+
 		case VK_RIGHT:
-			if (curPX + width > SCR_WIDTH - 17) break;
+			if (bigRC1.right > SCR_WIDTH - 17) break;
 
-			if (OnCollisionBox(VK_RIGHT, rc1, rc2))
+			MoveToRect(VK_RIGHT, &bigRC1);
+			MoveToRect(VK_RIGHT, &smallRC);
+
+			if (abs(bigRC1.right - bigRC2.left) <= dist && (bigRC1.bottom > bigRC2.top) && (bigRC1.top < bigRC2.bottom))
 			{
-				curEX += dist;
+				TempRectAngle(&bigRC1, &bigRC2);
+				smallRC = { bigRC1.left + sWidth / 2, bigRC1.top + sHeight / 2, bigRC1.right - sWidth / 2, bigRC1.bottom - sHeight / 2 };
 			}
-			curPX += dist;
-
 			break;
 		}
 
@@ -378,3 +197,108 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
 
 	return DefWindowProc(hWnd, iMessage, wParam, lParam);
 }
+
+#else
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
+{
+	HDC hdc;
+	PAINTSTRUCT ps;
+
+	switch (iMessage)
+	{
+	case WM_CREATE:
+		break;
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+
+		DrawRectMake(hdc, bigRC1);
+		DrawRectMake(hdc, bigRC2);
+		DrawRectMake(hdc, smallRC);
+
+		EndPaint(hWnd, &ps);
+		break;
+	case WM_KEYDOWN:
+	{
+		switch (wParam)
+		{
+		case VK_UP:
+			if (bigRC1.top < 1) break;
+
+			if (bigRC1.bottom == smallRC.bottom)
+			{
+				MoveToRect(VK_UP, &smallRC);
+			}
+
+			MoveToRect(VK_UP, &bigRC1);
+
+			if (abs(bigRC1.top - bigRC2.bottom) <= dist && (bigRC1.left < bigRC2.right) && (bigRC1.right > bigRC2.left))
+			{
+				TempRectAngle(&bigRC1, &bigRC2);
+				smallRC = { bigRC1.left + sWidth / 2, bigRC1.top + sHeight / 2, bigRC1.right - sWidth / 2, bigRC1.bottom - sHeight / 2 };
+			}
+			break;
+
+		case VK_DOWN:
+			if (bigRC1.bottom > SCR_HEIGHT - 40) break;
+
+			if (bigRC1.top == smallRC.top)
+			{
+				MoveToRect(VK_DOWN, &smallRC);
+			}
+
+			MoveToRect(VK_DOWN, &bigRC1);
+
+			if (abs(bigRC1.bottom - bigRC2.top) <= dist && (bigRC1.left < bigRC2.right) && (bigRC1.right > bigRC2.left))
+			{
+				TempRectAngle(&bigRC1, &bigRC2);
+				smallRC = { bigRC1.left + sWidth / 2, bigRC1.top + sHeight / 2, bigRC1.right - sWidth / 2, bigRC1.bottom - sHeight / 2 };
+			}
+			break;
+
+		case VK_LEFT:
+			if (bigRC1.left < 1) break;
+
+			if (bigRC1.right == smallRC.right)
+			{
+				MoveToRect(VK_LEFT, &smallRC);
+			}
+
+			MoveToRect(VK_LEFT, &bigRC1);
+
+			if (abs(bigRC1.left - bigRC2.right) <= dist && (bigRC1.bottom > bigRC2.top) && (bigRC1.top < bigRC2.bottom))
+			{
+				TempRectAngle(&bigRC1, &bigRC2);
+				smallRC = { bigRC1.left + sWidth / 2, bigRC1.top + sHeight / 2, bigRC1.right - sWidth / 2, bigRC1.bottom - sHeight / 2 };
+			}
+			break;
+
+		case VK_RIGHT:
+			if (bigRC1.right > SCR_WIDTH - 17) break;
+
+			if (bigRC1.left == smallRC.left)
+			{
+				MoveToRect(VK_RIGHT, &smallRC);
+			}
+
+			MoveToRect(VK_RIGHT, &bigRC1);
+
+			if (abs(bigRC1.right - bigRC2.left) <= dist && (bigRC1.bottom > bigRC2.top) && (bigRC1.top < bigRC2.bottom))
+			{
+				TempRectAngle(&bigRC1, &bigRC2);
+				smallRC = { bigRC1.left + sWidth / 2, bigRC1.top + sHeight / 2, bigRC1.right - sWidth / 2, bigRC1.bottom - sHeight / 2 };
+			}
+			break;
+		}
+
+		InvalidateRect(hWnd, NULL, TRUE);
+		break;
+	}
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	return DefWindowProc(hWnd, iMessage, wParam, lParam);
+}
+
+#endif
